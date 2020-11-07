b30a69fe3bf3abe8fbfba6875f5c8753b29ff67d
Eranga Perera <45055405+eranga88@users.noreply.github.com>
2020-11-07 19:33:21 +0800

    Create main.py
    This is the starting point of the application

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import View
+
+# This is the starting point of the application
+
+# Please run this file to start the project
+
+window = tk.Tk()
+
+interface = View.Interface(window)
+
+window.mainloop()
